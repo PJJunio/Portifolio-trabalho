f74752657da6c7b3e6fe990023da4c544ab877fc
PJJunio <119122468+PJJunio@users.noreply.github.com>
2024-12-05 07:22:05 -0300

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,7 @@
+document.getElementById('botao').addEventListener('click', function () {
+    alert('Em desenvolvimento, em breve mais notícias, confia que não foi desanimo de criar o formulário ;>');
+});
+
+document.getElementById('botao2').addEventListener('click', function () {
+    alert('Em desenvolvimento, infelizmente não tenho autorização para comentar mais sobre o projeto, fique ligado que logo logo teremos mais notícias');
+});
